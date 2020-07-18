90d4a9ef67822ad3a8ddcbd253495529d3d59c39
MD Shofiqul Alam <shofiqul.alam@grameenphone.com>
2020-07-18 17:51:14 +0600

    added cert for https

--- backend/kudocards/src/main.ts
+++ backend/kudocards/src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
 import { join } from 'path';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+  const httpsOptions = {
+    key: fs.readFileSync(join(__dirname, '..', 'cert/agilibo.key')),
+    cert: fs.readFileSync(join(__dirname, '..', 'cert/agilibo.crt')),
+  };
+  
+  
+ 
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
+
